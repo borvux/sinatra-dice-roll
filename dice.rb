d029d1a1c1b91abf82883e7950be56e7708c5172
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-02-21 23:22:49 +0000

    added html code to link the routes

--- dice.rb
+++ dice.rb
@@ -8,7 +8,21 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "Hello World"
+  "<h1>Dice Roll</h1>
+  <ul>
+    <li>
+      <a href = \"/dice/2/6\"> Roll two 6-sided dice</a>
+    </li>
+    <li>
+      <a href = \"/dice/2/10\"> Roll two 10-sided dice</a>
+    </li>
+    <li>
+      <a href = \"/dice/1/20\"> Roll one 20-sided dice</a>
+    </li>
+    <li>
+      <a href = \"/dice/5/4\"> Roll five 4-sided dice</a>
+    </li>
+  </ul>"
 end
 
 get("/zebra") do
@@ -61,4 +75,7 @@ get("/dice/5/4") do
   sum = first_die + second_die + third_die + fourth_die + fifth_die
 
   outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+
+  "<h1>5d4</h1>
+   <p>#{outcome}</p>"
 end
