eb1113c68f5b65bde0beecbc9c1365ba50f6d2b2
Benny Joram <80184074+borvux@users.noreply.github.com>
2024-02-22 03:47:05 +0000

    added views and updated the routes

--- dice.rb
+++ dice.rb
@@ -8,21 +8,22 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "<h1>Dice Roll</h1>
-  <ul>
-    <li>
-      <a href = \"/dice/2/6\"> Roll two 6-sided dice</a>
-    </li>
-    <li>
-      <a href = \"/dice/2/10\"> Roll two 10-sided dice</a>
-    </li>
-    <li>
-      <a href = \"/dice/1/20\"> Roll one 20-sided dice</a>
-    </li>
-    <li>
-      <a href = \"/dice/5/4\"> Roll five 4-sided dice</a>
-    </li>
-  </ul>"
+  # "<h1>Dice Roll</h1>
+  # <ul>
+  #   <li>
+  #     <a href = \"/dice/2/6\"> Roll two 6-sided dice</a>
+  #   </li>
+  #   <li>
+  #     <a href = \"/dice/2/10\"> Roll two 10-sided dice</a>
+  #   </li>
+  #   <li>
+  #     <a href = \"/dice/1/20\"> Roll one 20-sided dice</a>
+  #   </li>
+  #   <li>
+  #     <a href = \"/dice/5/4\"> Roll five 4-sided dice</a>
+  #   </li>
+  # </ul>"
+  erb(:elephant)
 end
 
 get("/zebra") do
@@ -34,48 +35,99 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
+  # first_die = rand(1..6)
+  # second_die = rand(1..6)
+  # sum = first_die + second_die
 	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  # @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
 	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
+  # "<h1>2d6</h1>
+  #  <p>#{outcome}</p>"
+  #erb(:two_six)
+  erb(:two_six)
 end
 
 # simulate two 10-sided dice
 get("/dice/2/10") do
-  first_die = rand(1..10)
-  second_die = rand(1..10)
-  sum = first_die + second_die
+  # first_die = rand(1..10)
+  # second_die = rand(1..10)
+  # sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  # @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d10</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  2.times do
+    die = rand(1..10)
+
+    @rolls.push(die)
+  end
+
+  # "<h1>2d10</h1>
+  #  <p>#{outcome}</p>"
+  erb(:two_ten)
 end
 
 # simulate one 20-sided die
 get("/dice/1/20") do
-  die = rand(1..20)
+  # @die = rand(1..20)
   
-  outcome = "The total is #{die}."
-  "<h1>1d20</h1>
-   <p>#{outcome}</p>"
+  # @outcome = "The total is #{die}."
+  # "<h1>1d20</h1>
+  #  <p>#{outcome}</p>"
+  @rolls = []
+
+  1.times do
+    die = rand(1..20)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_twenty)
 end
 
 # simulate five 4-sided dice
 get("/dice/5/4") do
-  first_die = rand(1..4)
-  second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
+  # first_die = rand(1..4)
+  # second_die = rand(1..4)
+  # third_die = rand(1..4)
+  # fourth_die = rand(1..4)
+  # fifth_die = rand(1..4)
+  # sum = first_die + second_die + third_die + fourth_die + fifth_die
 
-  outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
+  # @outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
 
-  "<h1>5d4</h1>
-   <p>#{outcome}</p>"
+  @rolls = []
+
+  5.times do
+    die = rand(1..4)
+
+    @rolls.push(die)
+  end
+
+  # "<h1>5d4</h1>
+  #  <p>#{outcome}</p>"
+  erb(:five_four)
+end
+
+# /dice.rb
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
